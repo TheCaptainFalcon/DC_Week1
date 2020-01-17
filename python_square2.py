facdf4e084f18217918ad59b9b72a2b7a6c9573c
Joseph Kim <josephkim570@gmail.com>
2020-01-17 15:41:15 -0500

    python_square2.py

--- python_square2.py
+++ python_square2.py
@@ -1,8 +1,19 @@
 import numpy
+import math
 
 #prompt for var N - equally stacked array
 #N -> (row * column)
-N = int(input("How big is the square?"))
+n = int(input("How big is the square? "))
 
-array1 = numpy.repeat(["*"], N).reshape(N, N)
-print(str(array1))
+#user input * itself
+n_ex = math.pow(n,2)
+
+#in order to maintain type balance...
+#* is contant and must be a string
+#var N must be an integer
+array1 = numpy.repeat("*", n_ex)
+array2 = array1.reshape(n,n)
+print(array2)
+
+#if you can do n^2 of repeat, it could solve the reshape issue
+#ie. user input = 7 -> 49 .shape(7,7)
