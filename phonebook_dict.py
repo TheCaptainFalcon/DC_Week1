1ad0bb28d3f2c508bd26015da6c39eb1483015b0
Joseph Kim <josephkim570@gmail.com>
2020-01-17 18:16:04 -0500

    a

---
+++ phonebook_dict.py
@@ -0,0 +1,20 @@
+phonebook_dict = {
+  'Alice': '703-493-1834',
+  'Bob': '857-384-1234',
+  'Elizabeth': '484-584-2923'
+}
+
+#print Elizabeth's phone number
+print("Elizabeth: " + "%s" % (phonebook_dict['Elizabeth']))
+
+#add an entry into the current dictionary - Kareem: 938-489-1234
+phonebook_dict ["Kareem"] = "938-489-1234"
+
+#delete Alice's phone entry
+del phonebook_dict ["Alice"]
+
+#change(add) Bob's phone number to 968-345-2345
+phonebook_dict["Bob"] = "968-345-2345"
+
+#print all the phone entries in dictionary
+print(phonebook_dict)
